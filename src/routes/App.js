151a943ed07df6305d39c561690342c95a5da8e3
openXapps <gavin.dalton1@gmail.com>
2022-09-28 09:52:46 +0200

    Initial commit

---
+++ src/routes/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <Container>
+      <Typography>App Home</Typography>
+    </Container>
+  );
+}
+
+export default App;
