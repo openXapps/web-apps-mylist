b5f529f8e9db5f44f0321f26779700454bf8d2de
openXapps <gavin.dalton1@gmail.com>
2023-05-05 15:23:35 +0200

    Dev progress

--- src/routes/Edit.jsx
+++ src/routes/Edit.jsx
@@ -1,15 +1,22 @@
-import { useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
+import { useLiveQuery } from 'dexie-react-hooks';
 
 // MUI Components
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 
+// App Specific
+import { AppContext } from '../context/AppStore';
+
 function Edit() {
+  // const smallScreen = useMediaQuery(theme => theme.breakpoints.down('sm'));
   // const rrNavigate = useNavigate();
   const rrPath = useLocation().pathname;
   const { listId } = useParams();
-  // const smallScreen = useMediaQuery(theme => theme.breakpoints.down('sm'));
+  const [{ db }] = useContext(AppContext);
+  // const [items, setItems] = useState([]);
+  const itemQuery = useLiveQuery(() => db.item.where('listId').equals(listId ? Number(listId) : 0).toArray());
 
   useEffect(() => {
     console.log(rrPath);
@@ -20,6 +27,8 @@ function Edit() {
     if (rrPath !== '/edit/' && listId) {
       // setEditMode('EDIT');
       // setRouteHeader('Edit Password');
+      // setItems()
+      console.log(itemQuery);
     }
 
     return () => true;
